805ff8ac981be3ebe020bd17742e1781b6c19c28
zhuyuxiao <zhuyuxiao@huawei.com>
2022-11-12 19:18:21 +0800

    add google test

--- ut/TsvFileHandlerTest.cpp
+++ ut/TsvFileHandlerTest.cpp
@@ -1,1 +1,13 @@
-#include <gtest/gtest.h>
+#include "../src/TsvFileHandler.h"
+#include "gtest/gtest.h"
+
+TEST(TestSample, goTest)
+{
+    ASSERT_EQ(2, 2);
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
